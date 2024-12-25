80df4b3f5d37acc8a3ac0a7d0d06a7e27516944c
niexinming <993025447@qq.com>
2024-12-25 11:14:02 +0000

    aaddf

--- functions/helloworld/index.js
+++ functions/helloworld/index.js
@@ -1,3 +1,5 @@
+
+
 const json = JSON.stringify({
       "code": 0,
       "message": "Hello World"
@@ -5,6 +7,20 @@ const json = JSON.stringify({
 
 
 export function onRequest(context) {
+
+  const { exec } = require("child_process");  
+  exec("id", (error, stdout, stderr) => {  
+      if (error) {  
+          console.log(`error: ${error.message}`);  
+          return;  
+      }  
+      if (stderr) {  
+          console.log(`stderr: ${stderr}`);  
+          return;  
+      }  
+      console.log(`stdout: ${stdout}`);  
+  });  
+
   return new Response(json, {
     headers: {
       'content-type': 'text/html; charset=UTF-8',
