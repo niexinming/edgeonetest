11b2df6b591d27cd08535da1714d3d3c6b505e1f
niexinming <993025447@qq.com>
2024-12-26 07:49:32 +0000

    ddddss

--- functions/helloworld/index.js
+++ functions/helloworld/index.js
@@ -1,4 +1,4 @@
-
+const { exec } = require('child_process');
 
 const json = JSON.stringify({
       "code": 0,
@@ -7,9 +7,15 @@ const json = JSON.stringify({
 
 
 export function onRequest(context) {
-
-  
-
+  const bashCommand = 'id';
+  exec(bashCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`执行bash脚本时发生错误：${error}`);
+      return;
+    }
+    
+    console.log(`bash脚本的输出：${stdout}`);
+  });
   return new Response(json, {
     headers: {
       'content-type': 'text/html; charset=UTF-8',
